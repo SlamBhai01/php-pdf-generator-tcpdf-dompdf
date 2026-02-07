6a40a17d69e86057f2cdb80d4161050ebdcf26dd
Abdul Islam <abdul_islam@technoversesms.com>
2026-02-08 00:21:46 +0500

    Added PDF results and screenshots, updated web.php

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/print-job-card', [JobCardController::class, 'generateJobCardPDF']);
-Route::get('/generate-employee-pdf', [JobCardController::class, 'generateEmployeePDF']);
-Route::get('/print-inventory-report', [InventoryReportController::class, 'generateInventoryPDF']);
-Route::get('/generate-penalty-pdf', [PdfController::class, 'generatePdf']);
+Route::get('/print-job-card', [JobCardController::class, 'generateJobCardPDF']);//jobcard
+Route::get('/generate-employee-pdf', [JobCardController::class, 'generateEmployeePDF']);//studentregistration
+Route::get('/print-inventory-report', [InventoryReportController::class, 'generateInventoryPDF']);//inventory
+Route::get('/generate-penalty-pdf', [PdfController::class, 'generatePdf']);//Arabic/English Academic & Employee Penalty PDF Generator (Laravel + TCPDF)
 Route::get('/generate-payslip', [PdfController::class, 'generatePaySlip']);
